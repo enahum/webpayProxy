0a05757dfd1046a422ab902e8d20761fabb83a3a
Elias Nahum <nahumhbl@gmail.com>
2015-06-08 16:13:30 -0300

    fix process form to work with firefox

--- js/process.js
+++ js/process.js
@@ -1,11 +1,12 @@
 $(document).ready(function(){
     var redirectWithError = function(data) {
-        form = '<form action="' + data.url + '" method="POST" name="confirm" style="display:none;"> ' +
+        var form = $('<form action="' + data.url + '" method="POST" name="confirm" style="display:none;"> ' +
         '<input type="hidden" id="token_ws" name="token_ws" value="' + $('#token_ws').val() + '" />'+
         '<input type="hidden" id="sessionId" name="sessionId" value="' + data.sessionId + '" />'+
         '<input type="hidden" id="error" name="error" value="' + data.error + '" />'+
-        '</form>';
-        $(form).submit();
+        '</form>');
+        $('body').append(form);
+        form.submit();
     };
 
     $.post('/transaction/result', {token_ws: $('#token_ws').val() })
@@ -15,11 +16,12 @@ $(document).ready(function(){
                 detail = response.detailOutput;
                 if (detail) {
                     session = response.urlRedirection.indexOf('voucher') < 0 ? '<input type="hidden" id="sessionId" name="sessionId" value="' + response.sessionId + '" />' : '';
-                    form = '<form action="' + response.urlRedirection + '" method="POST" name="confirm" style="display:none;"> ' +
+                    var form = $('<form action="' + response.urlRedirection + '" method="POST" name="confirm" style="display:none;"> ' +
                     '<input type="hidden" id="token_ws" name="token_ws" value="' + $('#token_ws').val() + '" />'+
                     session +
-                    '</form>';
-                    $(form).submit();
+                    '</form>');
+                    $('body').append(form);
+                    form.submit();
                 } else {
                     return redirectWithError({
                         url: response.urlRedirection,
